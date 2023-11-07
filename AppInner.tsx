0c313400dab3cc13e1deeb0a4ecf53f3bf40fe9f
plu457 <ekgus5095@gmail.com>
2023-11-07 17:33:46 +0900

    feat: 안드로이드 뒤로가기 기능 수정

--- AppInner.tsx
+++ AppInner.tsx
@@ -1,36 +1,78 @@
-import React, {useRef} from 'react';
-import {SafeAreaView} from 'react-native';
-import WebView, {WebViewMessageEvent} from 'react-native-webview';
+import React, {useRef, useState, useEffect} from 'react';
+import {SafeAreaView, BackHandler} from 'react-native';
+import WebView from 'react-native-webview';
 import usePermission from './src/hooks/usePermission';
 import useToken from './src/hooks/useToken';
+import Config from 'react-native-config';
 
 const AppInner = () => {
-  const webviewRef = useRef<WebView | null>(null);
-  // const site = 'https://soonsool.vercel.app';
-  const site = 'https://4cbf-211-197-13-149.ngrok-free.app';
-  const handleSetRef = (ref: WebView | null) => {
-    webviewRef.current = ref;
-  };
+  const webviewRef = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
 
-  const handleOnMessage = ({nativeEvent}: WebViewMessageEvent) => {
-    const {data} = nativeEvent;
-    console.log(data);
-  };
+  const BASE_URL = Config.BASE_URL;
 
   usePermission();
   useToken(webviewRef);
 
+  useEffect(() => {
+    const backAction = () => {
+      if (webviewRef.current && canGoBack) {
+        webviewRef.current.goBack();
+        return true;
+      }
+      return false;
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', backAction);
+    };
+  }, [canGoBack]);
+
+  const handleOnMessage = event => {
+    const {data} = event.nativeEvent;
+    if (data === 'navigationStateChange') {
+      setCanGoBack(event.nativeEvent.canGoBack);
+    }
+    console.log(data);
+  };
+
+  const handleSetRef = _ref => {
+    webviewRef.current = _ref;
+  };
+
+  const injectedJavaScript = `
+    (function() {
+      function wrap(fn) {
+        return function wrapper() {
+          var res = fn.apply(this, arguments);
+          window.ReactNativeWebView.postMessage('navigationStateChange');
+          return res;
+        }
+      }
+
+      history.pushState = wrap(history.pushState);
+      history.replaceState = wrap(history.replaceState);
+      window.addEventListener('popstate', function() {
+        window.ReactNativeWebView.postMessage('navigationStateChange');
+      });
+    })();
+
+    true;
+  `;
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
       <WebView
-        source={{uri: site}}
-        userAgent="Mozilla/5.0 (Linux; Win64; x64; rv:46.0)r Gecko/20100101 Firefox/68.0"
+        source={{uri: BASE_URL}}
+        ref={handleSetRef}
+        onMessage={handleOnMessage}
+        injectedJavaScript={injectedJavaScript}
         originWhitelist={['https://*', 'http://*', 'file://*', 'sms://*']}
         allowsInlineMediaPlayback={true}
         mediaPlaybackRequiresUserAction={false}
-        onMessage={handleOnMessage}
         allowsBackForwardNavigationGestures
-        ref={handleSetRef}
       />
     </SafeAreaView>
   );
